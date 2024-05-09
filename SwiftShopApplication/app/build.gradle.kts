d323494a375386ce46b7c0d30f5d3634a4229eca
Bora Yetkin <borayetkin@gmail.com>
2024-05-09 23:27:08 +0300

    Login and Registration Screens added

--- SwiftShopApplication/app/build.gradle.kts
+++ SwiftShopApplication/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -37,6 +38,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
