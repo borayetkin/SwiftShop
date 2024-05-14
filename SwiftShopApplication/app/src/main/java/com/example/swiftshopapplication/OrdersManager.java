6c786fc0d8e41105311fe1e58323e27dff690797
1enesozdemir1 <91872892+1enesozdemir1@users.noreply.github.com>
2024-05-14 09:01:35 +0300

    Added the order functionality

---
+++ SwiftShopApplication/app/src/main/java/com/example/swiftshopapplication/OrdersManager.java
@@ -0,0 +1,29 @@
+package com.example.swiftshopapplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrdersManager {
+    private static OrdersManager instance;
+    private List<Product> orders;
+
+    private OrdersManager() {
+        orders = new ArrayList<>();
+    }
+
+    public static OrdersManager getInstance() {
+        if (instance == null) {
+            instance = new OrdersManager();
+        }
+        return instance;
+    }
+
+    public void addToOrders(List<Product> products) {
+        orders.addAll(products);
+    }
+
+    public List<Product> getOrders() {
+        return orders;
+    }
+}
+
---
+++ SwiftShopApplication/app/src/main/java/com/example/swiftshopapplication/ui/orders/OrdersFragment.java
@@ -0,0 +1,30 @@
+package com.example.swiftshopapplication.ui.orders;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.swiftshopapplication.OrdersManager;
+import com.example.swiftshopapplication.ProductsAdapter;
+import com.example.swiftshopapplication.R;
+
+public class OrdersFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_orders, container, false);
+
+        recyclerView = view.findViewById(R.id.ordersRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new ProductsAdapter(OrdersManager.getInstance().getOrders(), getContext()));
+
+        return view;
+    }
+}
+
