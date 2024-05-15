bf3eb70e0300acae01437518eb1c5a6f70fa5c2d
Halil Avcı <halilavci@proton.me>
2024-05-15 10:43:42 +0300

    Password hiding feature added

--- SwiftShopApplication/app/src/main/java/com/example/swiftshopapplication/LoginActivity.java
+++ SwiftShopApplication/app/src/main/java/com/example/swiftshopapplication/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.swiftshopapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -51,6 +52,7 @@ public class LoginActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         editTextEmail = findViewById(R.id.email);
         editTextPassword = findViewById(R.id.password);
+        editTextPassword.setTransformationMethod(new PasswordTransformationMethod());
         buttonLogin = findViewById(R.id.button_login);
         progressBar = findViewById(R.id.progressBar);
         textView = findViewById(R.id.registerNow);
